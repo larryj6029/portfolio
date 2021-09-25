e9d537312d0697bc54c0cbe62021f2cff54c884b
Larry San Nicolas <larryjsannicolas@gmail.com>
2021-09-25 16:41:13 -0700

    added meta description for seo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,16 @@ import { Helmet } from 'react-helmet'
 export default function Layout(props) {
     return (
         <>
-            <Helmet title="Portfolio" defer={false} />
+            <Helmet title="Portfolio" defer={false} meta={[
+                {
+                    name: 'description',
+                    content: "This is a portfolio of projects completed by Lawrence San Nicolas.",
+                },
+                {
+                    name: 'keywords',
+                    content: "Software, portfolio, web development, react",
+                },
+            ]} />
             <div className="layout">
                 <Navbar />
                 <div className="content">
